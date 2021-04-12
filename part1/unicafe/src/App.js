52bf9704bd44dcb5581aa3ab508f90f33869ab5e
Cisse23 <baker.cisse@gmail.com>
2021-04-12 14:10:58 +0300

    unicafe steps 1.6-1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,28 @@
 import React, { useState } from 'react'
+
+
+const Statistics = (props) => {
+    const all = props.good + props.bad + props.neutral
+    const average = (1 * props.good + 0 * props.neutral + -1 * props.bad) / all
+    const positive = props.good / all * 100
+
+    if (all === 0) {
+        return <p>No feedback received</p>
+    }
+    return (
+        <div>
+            <h1>Statistics</h1>
+            <p>Good: {props.good} </p>
+            <p>Neutral: {props.neutral} </p>
+            <p>Bad: {props.bad} </p>
+            <p>All: {all} </p>
+            <p>Average: {average} </p>
+            <p>Positive: {positive}</p>
+        </div>
+    )
+}
+
+
 const App = () => {
 
     const [good, setGood] = useState(0)
@@ -8,8 +32,15 @@ const App = () => {
     return (
         <div>
             <h1>Please give feedback</h1>
+
+            <button onClick={() => setGood(good + 1)} >Good</button>
+            <button onClick={() => setNeutral(neutral + 1)} >Neutral</button>
+            <button onClick={() => setBad(bad + 1)} >Bad</button>
+
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
 
 export default App
+
